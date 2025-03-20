63d3cece65fadbe4e4f02d82ae283c7c19790ce3
dirtyscream <itdanielwork@gmail.com>
2025-03-20 14:35:59 +0300

    lab2

--- lab1/dirwalk.c
+++ lab1/dirwalk.c
@@ -5,6 +5,7 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <limits.h>
+#include <locale.h>
 #include <errno.h>
 #include "array_entries.h"
 
@@ -19,11 +20,11 @@ void add_entry(ArrayEntries *array, char* full_path, int flag) {
     array->entries[array->size - 1].flag = flag;
 }
 
-int compare_entries(const void *a, const void *b) {
+int compare_for_sorting(const void *a, const void *b) {
     return strcoll(((Map *)a)->entry, ((Map *)b)->entry);
 }
 
-void scan_directory(const char *dir_path, int show_links, int show_dirs, int show_files, ArrayEntries *array_entries, int *entry_count) {
+void dirwalk(const char *dir_path, int flag_links, int flag_dirs, int flag_files, ArrayEntries *array_entries, int *entry_count) {
     DIR *dir = opendir(dir_path);
     if (!dir) {
         perror("opendir");
@@ -50,38 +51,47 @@ void scan_directory(const char *dir_path, int show_links, int show_dirs, int sho
             flag = 2;
         }
 
-        if ((show_links && flag == 0) || (show_dirs && flag == 2) || (show_files && flag == 1) || (!show_links && !show_dirs && !show_files)) {
+        if ((flag_links && flag == 0) || (flag_dirs && flag == 2) || (flag_files && flag == 1) || (!flag_links && !flag_dirs && !flag_files)) {
             add_entry(array_entries, full_path, flag);
         }
 
         if (flag == 2) {
-            scan_directory(full_path, show_links, show_dirs, show_files, array_entries, entry_count);
+            dirwalk(full_path, flag_links, flag_dirs, flag_files, array_entries, entry_count);
         }
     }
     closedir(dir);
 }
 
 int main(int argc, char *argv[]) {
-    int show_links = 0, show_dirs = 0, show_files = 0, sort_output = 0;
+    setlocale(LC_COLLATE, "");
+    int flag_links = 0, flag_dirs = 0, flag_files = 0, sort_output = 0;
     ArrayEntries array_entries = {0, NULL};
     int entry_count = 0;
     int opt;
 
     while ((opt = getopt(argc, argv, "ldfs")) != -1) {
         switch (opt) {
-            case 'l': show_links = 1; break;
-            case 'd': show_dirs = 1; break;
-            case 'f': show_files = 1; break;
-            case 's': sort_output = 1; break;
+            case 'l': 
+                flag_links = 1; 
+                break;
+            case 'd': 
+                flag_dirs = 1; 
+                break;
+            case 'f': 
+                flag_files = 1; 
+                break;
+            case 's': 
+                sort_output = 1; 
+                break;
             default: 
                 fprintf(stderr, "Usage: %s [-l] [-d] [-f] [-s] [directory]\n", argv[0]); 
                 exit(EXIT_FAILURE);
         }
-    }
+    }  
     const char *start_dir = (optind < argc) ? argv[optind] : ".";
-    scan_directory(start_dir, show_links, show_dirs, show_files, &array_entries, &entry_count);
+    dirwalk(start_dir, flag_links, flag_dirs, flag_files, &array_entries, &entry_count);
     if (sort_output) {
-        qsort(array_entries.entries, array_entries.size, sizeof(Map), compare_entries);
+        qsort(array_entries.entries, array_entries.size, sizeof(Map), compare_for_sorting);
     }
     for (int i = 0; i < array_entries.size; i++) {
         printf("%s: %s\n", (array_entries.entries[i].flag == 0) ? "Symlink" :
